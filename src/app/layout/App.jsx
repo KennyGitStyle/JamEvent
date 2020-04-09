f94d2f7024340bee5e0c0d1b8758dff11c9012db
Kenneth <kennethhendricks10@gmail.com>
2020-04-09 13:54:32 +0100

    React Markup

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,21 @@
+import React, {Component, Fragment} from 'react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import Navbar from '../../features/nav/Navbar'
+import { Container } from 'semantic-ui-react'
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Navbar />
+                <Container className="main">
+                    <EventDashboard />
+                </Container>
+            </Fragment>
+            
+        )
+    }
+
+}
+
+export default App
